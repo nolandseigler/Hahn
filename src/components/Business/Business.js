fcf2477154b7694175ef0bbcc7e9e05d097673fb
Noland Seigler <nolandseigler@gmail.com>
2020-05-12 23:30:05 -0500

    Business component complete. BusinessList Component started

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './Business.css';
 const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
     name: 'MarginOtto Pizzeria',
@@ -37,3 +37,8 @@ class Business extends React.Component {
         );
     }
 }
+
+//tutorial way is below
+// export default Business;
+//below is my way
+module.exports = Business;
