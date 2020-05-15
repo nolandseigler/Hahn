1dcaeefafddaa0e4a5fc8ef576fc189afc61d418
Noland Seigler <nolandseigler@gmail.com>
2020-05-14 23:45:31 -0500

    corrected error in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
@@ -7,7 +8,7 @@ const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
     name: 'MarginOtto Pizzeria',
     address: '1010 Paddington Way',
-    city: 'Flavortown',
+    city: 'Bordertown',
     state: 'NY',
     zipCode: '10101',
     category: 'Italian',
@@ -24,14 +25,16 @@ const businesses = [
     business
 ];
 
-function App() {
-  return (
-      <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList businesses={businesses} />
-      </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>ravenous</h1>
+                <SearchBar />
+                <BusinessList businesses={businesses} />
+            </div>
+        );
+    }
 }
 
 export default App;
